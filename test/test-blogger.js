a6959e6e3fb923ca9e04fb534fb3c21fefcdd1e3
k <lookerkj@protonmail.com>
2018-04-24 10:42:51 -0700

    added chai

---
+++ test/test-blogger.js
@@ -0,0 +1,7 @@
+//import chai, declare expect variable
+const expect = require('chai').expect;
+
+//import bloggerRouter (this has all the stuff we need to test)
+const bloggerRouter = require('../bloggerRouter');
+
+//unit tests for our blogger function
